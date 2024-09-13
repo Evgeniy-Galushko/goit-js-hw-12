cb86debb021acfbdae2c3b213cec39b8dfbbe5a7
Galushko Evgeniy <yudginchik@gmail.com>
2024-09-13 13:24:11 +0300

    add

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -36,30 +36,7 @@ export default function gallerys(imgs) {
   }
 }
 
-// export function paginationRender(imgs) {
-//   if (imgs.length === 0) {
-//     iziToast.warning({
-//       backgroundColor: '#EF4040',
-//       position: 'center',
-//       iconUrl: sorry,
-//       message:
-//         'Sorry, there are no images matching your search query. Please try again!',
-//     });
-//   }
-//   const line = imgs
-//     .map(
-//       img => `<li class="gallery-card">
-//         <a class="gallery-link" href=${img.largeImageURL}>
-//           <img src=${img.webformatURL} data-source=${img.largeImageURL} alt=${img.tags} width="360">
-//         </a>
-//           <ul class="gallery-card-signature">
-//             <li><p class="gallery-card-signature-p">Likes</p><p class="gallery-card-signature-number">${img.likes}</p></li>
-//             <li><p class="gallery-card-signature-p">Views</p><p class="gallery-card-signature-number">${img.views}</p></li>
-//             <li><p class="gallery-card-signature-p">Comments</p><p class="gallery-card-signature-number">${img.comments}</p></li>
-//             <li><p class="gallery-card-signature-p">Downloads</p><p class="gallery-card-signature-number">${img.downloads}</p></li>
-//           </ul>
-//         </li>`
-//     )
-//     .join('');
-//   gallery.insertAdjacentHTML('beforeend', line);
-// }
+// setTimeout(() => {
+//   window.scrollBy(0, 436);
+// }, 200);
+// window.scrollBy(164, 436);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import request from './js/pixabay-api';
@@ -5,6 +7,7 @@ import gallerys from './js/render-functions';
 
 const forms = document.querySelector('.search');
 const loader = document.querySelector('.loader');
+const loaders = document.querySelector('.loaders');
 const simpleBox = new SimpleLightbox('.gallery a', {
   captions: true,
   captionsData: 'alt',
@@ -24,21 +27,32 @@ function searchText(event) {
   const textSearch = text.toLowerCase();
   textQuery = textSearch;
   if (textSearch === '') {
-    return;
+    iziToast.warning({
+      backgroundColor: '#F4A460',
+      position: 'center',
+      message: "Sorry, you haven't entered anything!",
+    });
+    button.disabled = true;
   }
 
   loader.classList.toggle('js-non-display');
 
   request(textSearch, pages).then(response => {
     const imgs = response.data.hits;
-    const numberOfCards = response.data.total;
+    console.log(imgs);
 
     gallerys(imgs);
-    loader.classList.toggle('js-non-display');
     buttonLoad.classList.add('js-button-load');
+    if (imgs.length === 0) {
+      buttonLoad.classList.toggle('js-button-load');
+    }
+    loader.classList.toggle('js-non-display');
 
     simpleBox.refresh();
-    // return imgs;
+    // const galleryCard = document.querySelector('.gallery-card');
+    // const domRect = galleryCard.getBoundingClientRect();
+    // window.scrollBy(0, `${domRect.height * 2}`);
+    // console.log(domRect);
   });
 
   // forms.reset();
@@ -49,12 +63,28 @@ forms.addEventListener('submit', searchText);
 async function reloadingCards(event) {
   try {
     pages += 1;
+    loaders.classList.toggle('js-non-display-pagination');
+    buttonLoad.classList.toggle('js-button-load');
     const posts = await request(textQuery, pages);
     gallerys(posts.data.hits);
+    loaders.classList.toggle('js-non-display-pagination');
 
-    console.log(pages);
+    const numberOfPictures = posts.data.total;
+    const numberOfPages = Math.ceil(numberOfPictures / perPage);
+    simpleBox.refresh();
+    if (numberOfPages <= pages) {
+      buttonLoad.classList.remove('js-button-load');
+      iziToast.warning({
+        backgroundColor: '#FF4500',
+        position: 'center',
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+    buttonLoad.classList.toggle('js-button-load');
 
-    console.log(posts.data.hits);
+    const galleryCard = document.querySelector('.gallery-card');
+    const domRect = galleryCard.getBoundingClientRect();
+    window.scrollBy(0, `${domRect.height * 2}`);
   } catch (error) {
     console.log(error);
   }
